1e900278641230f9465a108373ba8626499f4ff1
Stefan Gherman <stefangheman@gmail.com>
2019-10-30 10:38:46 +0200

    made chabges to the input + updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Binary to decimal duplex converter
+
+DISCLAIMER!
+It does not provide 2's complement conversions!
+
+
+Usage:
+Input a num and its base and press enter, let magic happen.
--- binary_nums.py
+++ binary_nums.py
@@ -19,27 +19,20 @@ def bin_to_dec(string):
     new_str = string[-1::-1]
 
     num = 0
-    #print(new_str)
     for i in range(len(new_str)):
         num += int(new_str[i]) * (2)**(i)
 
     return num    
 
+def main_exec():
+    in1 = input("Enter num and base with whitespace between them:")
 
-in1 = int(input("Enter decimal num:"))
+    in1 = in1.split()
 
+    if(in1[1] == "2"):
+        print(bin_to_dec(in1[0]), str(10))
 
+    elif(in1[1] == "10"):
+        print(dec_to_binary(int(in1[0])),str(2))    
 
-print(dec_to_binary(in1), str(2))
-
-
-in2 = input("Enter binary num:")
-
-
-print(bin_to_dec(in2), str(10))
-
-
-if "1000111100111" == "dec_to_binary(in1)":
-    print("True")
-else:
-    print("False")    
+main_exec()
