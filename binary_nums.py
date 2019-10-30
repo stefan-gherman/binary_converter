670a943f1f08e2aed0f9acf687464356240788f6
Stefan Gherman <stefangheman@gmail.com>
2019-10-30 10:58:01 +0200

    autopepped8 the script

--- binary_nums.py
+++ binary_nums.py
@@ -4,16 +4,17 @@ def dec_to_binary(num):
     bin_num = []
 
     while num // 2 > 0:
-        bin_num.append(num%2)
-        num = num // 2 
+        bin_num.append(num % 2)
+        num = num // 2
 
     bin_num.append(num)
     bin_num = bin_num[-1::-1]
 
-    num_ret =""
+    num_ret = ""
     for digit in bin_num:
-        num_ret+= str(digit) 
-    return num_ret 
+        num_ret += str(digit)
+    return num_ret
+
 
 def bin_to_dec(string):
     new_str = string[-1::-1]
@@ -22,7 +23,8 @@ def bin_to_dec(string):
     for i in range(len(new_str)):
         num += int(new_str[i]) * (2)**(i)
 
-    return num    
+    return num
+
 
 def main_exec():
     in1 = input("Enter num and base with whitespace between them:")
@@ -33,6 +35,7 @@ def main_exec():
         print(bin_to_dec(in1[0]), str(10))
 
     elif(in1[1] == "10"):
-        print(dec_to_binary(int(in1[0])),str(2))    
+        print(dec_to_binary(int(in1[0])), str(2))
+
 
 main_exec()
